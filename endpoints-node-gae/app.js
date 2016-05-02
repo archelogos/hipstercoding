d9ce8418fc2e776173edc5c88d31244633ecfe08
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-05-02 17:52:00 +0200

    V.0.0.22
    + Weak validation
    + Cors
    + Bugs Fixed
    + More Stable

--- endpoints-node-gae/app.js
+++ endpoints-node-gae/app.js
@@ -1,5 +1,7 @@
 /**
-* Server Side HC. Testing node running on GAE YIPE!
+* API NODEJS TO DATASTORE.
+* GOOGLE APP ENGINE
+* @author: @Sergio_Gordillo
 */
 
 'use strict';
@@ -7,87 +9,126 @@
 var express = require('express');
 var gcloud = require('gcloud');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var Event = require('./models/event');
 var Preference = require('./models/preference');
 
-
 var app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var router = express.Router();
 
 router.use(function(req, res, next) {
-  // do logging
-  console.log('Something is happening.');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(); // make sure we go to the next routes and don't stop here
 });
 
+var whitelist = ['http://hipstercoding.sergiogordillo.com', 'http://localhost:9000'];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
+};
+
+/****************************************************************
+******************************************************************/
+
+/**
+* Routing home. Dummy routing
+*/
 router.get('/', function(req, res) {
-    res.send('hipstercoding api');
+    res.send('NodeJS API. HipsterCoding. @Sergio_Gordillo');
 });
 
-/*
-* Insert event
+/**
+* @description: Insert Event. Get Method. Pixel Tracking
+* @param: route
+* @param: callback
+* Using Request > Query. Params _x, _y, _type
+* @return: JSON if OK, Error 400 if Bad Request, Random Datastore Error
 */
-router.post('/api/event/insert', function(req, res){
+//TODO: Handle Datastore errors
+router.get('/api/event/insert', cors(corsOptions), function(req, res){
+
+  //Weak Validation
+  if(typeof req.query._x === 'undefined' || typeof req.query._y === 'undefined'
+    || typeof req.query._type === 'undefined')
+      return res.status(400).send({code: 400, error: 'Bad Request'});
+
   var eventProperties = {
-    _x : req.body._x,
-    _y : req.body._y,
-    _type : req.body._type
+    _x : req.query._x,
+    _y : req.query._y,
+    _type : req.query._type
   };
-  Event.insertEvent(eventProperties, function(err) {
-    if (err)
-        res.send(err);
 
-    res.json({ message: 'OK' });
+  Event.insertEvent(eventProperties, function(err, entity) {
+    if (err)
+      return res.status(err.code).send(err);
+
+    res.json({ entity });
   });
+
 });
 
-/*
-* GetAll event
+/**
+* @description: GetAll Events. Get Method.
+* @param: route
+* @param: callback
+* @return: JSON if OK, Random Datastore Error
 */
-router.get('/api/event/getAll', function(req, res){
+//TODO: Handle Datastore errors
+router.get('/api/event/getAll', cors(corsOptions), function(req, res){
   Event.getAllEvents(function(err, entities){
     if (err)
-        res.send(err);
+      return res.status(err.code).send(err);
 
     res.json({ entities });
   });
 });
 
-/*
-* @description:
-* @params:
-* @return:
+/**
+* @description: Insert Preference. Post Method.
+* @param: route
+* @param: callback
+* @return: JSON if OK, Error 400 if Bad Request, Random Datastore Error
 */
-router.post('/api/preference/insert', function(req, res){
+//TODO: Handle Datastore errors
+router.post('/api/preference/insert', cors(corsOptions), function(req, res){
+
+  //Weak Validation
+  if(typeof req.body.animal === 'undefined')
+      return res.status(400).send({code: 400, error: 'Bad Request'});
+
   var preferenceProperties = {
     animal : req.body.animal
   };
-  Preference.insertPreference(preferenceProperties, function(err) {
-    if (err)
-        res.send(err);
 
-    res.json({ message: 'OK' });
+  Preference.insertPreference(preferenceProperties, function(err, entity) {
+    if (err)
+      return res.status(err.code).send(err);
+
+    res.json({ entity });
   });
 });
 
-router.get('/api/preference/getAll', function(req, res){
+/**
+* @description: GetAll Preferences. Get Method.
+* @param: route
+* @param: callback
+* @return: JSON if OK, Random Datastore Error
+*/
+//TODO: Handle Datastore errors
+router.get('/api/preference/getAll', cors(corsOptions), function(req, res){
   Preference.getAllPreferences(function(err, entities){
     if (err)
-        res.send(err);
+      return res.status(err.code).send(err);
 
     res.json({ entities });
   });
 });
 
-
 app.use('/', router);
-
 
 var port = process.env.PORT || 8080;        // set our port
 
